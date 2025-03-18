dee71718a95aa462843a2df4e6d33171b1b9d8a6
yumemisugi <theoriacode.yt@gmail.com>
2025-03-18 15:27:09 +0900

    ✨ addTaskでtodos.jsonにデータ追加されるようにした

--- src/app/components/AddTask.tsx
+++ src/app/components/AddTask.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+"use client";
+
+import { addTodo } from "@/api";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 
 const AddTask = () => {
+  const [taskTitle, setTaskTitle] = useState("");
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    await addTodo({ id: "4", text: taskTitle });
+
+    setTaskTitle("");
+  };
+
   return (
-    <form className="mb-4 space-y-3">
+    <form className="mb-4 space-y-3" onSubmit={handleSubmit}>
       <input
         type="text"
         className="w-full border px-4 py-2 rounded-lg focus:outline-none focus:border-blue-400"
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setTaskTitle(e.target.value)
+        }
+        value={taskTitle}
       />
       <button className="w-full px-4 py-2 text-white bg-blue-500 rounded transform hover:bg-blue-400 hover:scale-95 duration-200">
         Add Task
